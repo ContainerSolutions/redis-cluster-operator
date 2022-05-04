609f5f3a067b5612d1b727f3a48b9d58fd81ae6c
Christiaan Vermeulen <chrism.vermeulen@gmail.com>
2022-05-04 18:38:45 +0200

    Add test for statefulset during reconcile

--- controllers/rediscluster_controller_test.go
+++ controllers/rediscluster_controller_test.go
@@ -19,6 +19,8 @@ package controllers
 import (
 	"context"
 	cachev1alpha1 "github.com/containersolutions/redis-cluster-operator/api/v1alpha1"
+	v1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
@@ -75,5 +77,96 @@ func TestRedisClusterReconciler_Reconcile_ReturnsErrorIfCannotGetStatefulset(t *
 	_, err := r.Reconcile(context.TODO(), req)
 	if err != nil {
 		t.Fatalf("reconcile: (%v)", err)
+	}
+}
+
+func TestRedisClusterReconciler_Reconcile_CreatesStatefulsetIfDoesntExist(t *testing.T) {
+	// Register operator types with the runtime scheme.
+	s := scheme.Scheme
+	_ = cachev1alpha1.AddToScheme(s)
+
+	clientBuilder := fake.NewClientBuilder()
+	clientBuilder.WithObjects(&cachev1alpha1.RedisCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "redis-cluster",
+			Namespace: "default",
+		},
+	})
+	client := clientBuilder.Build()
+
+	// Create a ReconcileMemcached object with the scheme and fake client.
+	r := &RedisClusterReconciler{
+		Client: client,
+		Scheme: s,
+	}
+
+	// Mock request to simulate Reconcile() being called on an event for a
+	// watched resource .
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      "redis-cluster",
+			Namespace: "default",
+		},
+	}
+	_, err := r.Reconcile(context.TODO(), req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+
+	sts := &v1.StatefulSet{}
+	err = client.Get(context.TODO(), types.NamespacedName{
+		Name:      "redis-cluster",
+		Namespace: "default",
+	}, sts)
+	if err != nil {
+		t.Fatalf("Failed to fetch created Statefulset %v", err)
+	}
+}
+
+func TestRedisClusterReconciler_Reconcile_DoesNotFailIfStatefulsetExists(t *testing.T) {
+	// Register operator types with the runtime scheme.
+	s := scheme.Scheme
+	_ = cachev1alpha1.AddToScheme(s)
+
+	clientBuilder := fake.NewClientBuilder()
+	clientBuilder.WithObjects(&cachev1alpha1.RedisCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "redis-cluster",
+			Namespace: "default",
+		},
+	}, &v1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "redis-cluster",
+			Namespace: "default",
+		},
+	})
+	client := clientBuilder.Build()
+
+	// Create a ReconcileMemcached object with the scheme and fake client.
+	r := &RedisClusterReconciler{
+		Client: client,
+		Scheme: s,
+	}
+
+	// Mock request to simulate Reconcile() being called on an event for a
+	// watched resource .
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      "redis-cluster",
+			Namespace: "default",
+		},
+	}
+	_, err := r.Reconcile(context.TODO(), req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+
+	sts := &v1.StatefulSet{}
+	err = client.Get(context.TODO(), types.NamespacedName{
+		Name:      "redis-cluster",
+		Namespace: "default",
+	}, sts)
+	if err != nil {
+		t.Fatalf("Failed to fetch created Statefulset %v", err)
 	}
 }
