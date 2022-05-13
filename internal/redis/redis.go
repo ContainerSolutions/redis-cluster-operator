205d24e6a5d748bddb7b378c0e305c546bbbb1e1
Christiaan Vermeulen <chrism.vermeulen@gmail.com>
2022-05-13 17:43:51 +0200

    Add methods to manage redis nodes

--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -2,18 +2,91 @@ package redis
 
 import (
 	"context"
+	"errors"
 	"github.com/go-redis/redis/v8"
 	"strings"
 )
 
+// NodeAttributes represents the data returned from the CLUSTER NODES commands.
+// The format returned from Redis contains the fields, split by spaces
+//
+// <id> <ip:port@cport> <flags> <master> <ping-sent> <pong-recv> <config-epoch> <link-state> <slot> <slot> ... <slot>
+//
+// <id> represents the ID of the node in UUID format
+// <ip:port@cport> part has the IP and port of the redis server, with the gossip port after @
+// <flags> is a string of flags separated by comma (,). Useful flags include master|slave|myself. Myself is the indicator that this line is for the calling node
+// <master> represents the node ID that is being replicated, if the node is a slave. if it is not replicating anything it will be replaced by a dash (-)
 type NodeAttributes struct {
-	ID string
+	ID    string
+	host  string
+	port  string
+	flags []string
 }
 
+func NewNodeAttributes(nodeString string) NodeAttributes {
+	// Output Format: <0:id> <1:ip:port@cport> <2:flags> <3:master> <4:ping-sent> <5:pong-recv> <6:config-epoch> <7:link-state> <slot> <slot> ... <slot>
+	friendFields := strings.Split(nodeString, " ")
+	address := strings.Split(friendFields[1], "@")[0]
+	addressParts := strings.Split(address, ":")
+	return NodeAttributes{
+		ID:    friendFields[0],
+		host:  addressParts[0],
+		port:  addressParts[1],
+		flags: strings.Split(friendFields[2], ","),
+	}
+}
+
+func (n *NodeAttributes) HasFlag(flag string) bool {
+	for _, _flag := range n.flags {
+		if _flag == flag {
+			return true
+		}
+	}
+	return false
+}
+
+// Node represents a single Redis Node with a client, and a client builder.
+// The client builder is necessary in case we are getting nodes from this node, for example when we load friends.
+// We need a clientBuilder, so we can create the same base client for nodes fetched through this node,
+// for example getting all of the attched nodes.
+// This is especially useful for testing, as we need to pass in a mocked constructor for child clients.
 type Node struct {
 	*redis.Client
 	NodeAttributes NodeAttributes
-	clientBuilder func(opt *redis.Options) *redis.Client
+	clientBuilder  func(opt *redis.Options) *redis.Client
+}
+
+func NewNode(ctx context.Context, opt *redis.Options, clientBuilder func(opt *redis.Options) *redis.Client) (*Node, error) {
+	redisClient := clientBuilder(opt)
+	node := &Node{
+		Client:         redisClient,
+		NodeAttributes: NodeAttributes{},
+		clientBuilder:  clientBuilder,
+	}
+	attributes, err := node.GetSelfAttributes(ctx)
+	if err != nil {
+		return nil, err
+	}
+	node.NodeAttributes = attributes
+	return node, nil
+}
+
+func (n *Node) GetSelfAttributes(ctx context.Context) (NodeAttributes, error) {
+	nodes, err := n.ClusterNodes(ctx).Result()
+	if err != nil {
+		return NodeAttributes{}, err
+	}
+	for _, friendString := range strings.Split(nodes, "\n") {
+		if strings.TrimSpace(friendString) == "" {
+			continue
+		}
+		nodeAttributes := NewNodeAttributes(friendString)
+		if !nodeAttributes.HasFlag("myself") {
+			continue
+		}
+		return nodeAttributes, nil
+	}
+	return NodeAttributes{}, errors.New("could not find myself in nodes list")
 }
 
 // GetFriends returns a list of all the other Redis nodes that this node knows about
@@ -27,21 +100,16 @@ func (n *Node) GetFriends(ctx context.Context) ([]*Node, error) {
 		if strings.TrimSpace(friendString) == "" {
 			continue
 		}
-		// Output Format: <0:id> <1:ip:port@cport> <2:flags> <3:master> <4:ping-sent> <5:pong-recv> <6:config-epoch> <7:link-state> <slot> <slot> ... <slot>
-		friendFields := strings.Split(friendString, " ")
-
-		if strings.Contains(friendFields[2], "myself") {
+		nodeAttributes := NewNodeAttributes(friendString)
+		if nodeAttributes.HasFlag("myself") {
 			// We only want to return nodes which are friends not ourself
 			continue
 		}
-		address := strings.Split(friendFields[1], "@")[0]
 		result = append(result, &Node{
 			n.clientBuilder(&redis.Options{
-				Addr:               address,
+				Addr: nodeAttributes.host + ":" + nodeAttributes.port,
 			}),
-			NodeAttributes{
-				ID: friendFields[0],
-			},
+			nodeAttributes,
 			n.clientBuilder,
 		})
 	}
--- internal/redis/redis_test.go
+++ internal/redis/redis_test.go
@@ -7,6 +7,73 @@ import (
 	"testing"
 )
 
+// region NewNodeAttributes
+func TestNewNodeAttributes(t *testing.T) {
+	attributes := NewNodeAttributes("9fd8800b31d569538917c0aaeaa5588e2f9c6edf 10.244.0.218:6379@16379 myself,master - 0 1652373716000 0 connected")
+	if attributes.host != "10.244.0.218" || attributes.port != "6379" || attributes.ID != "9fd8800b31d569538917c0aaeaa5588e2f9c6edf" {
+		t.Fatalf("Attributes not being correctly extracted from node string")
+	}
+}
+
+// endregion
+
+// region NodeAttributes
+func TestNodeAttributes_HasFlag(t *testing.T) {
+	attributes := NewNodeAttributes("9fd8800b31d569538917c0aaeaa5588e2f9c6edf 10.244.0.218:6379@16379 myself,master - 0 1652373716000 0 connected")
+	if !attributes.HasFlag("myself") || !attributes.HasFlag("master") {
+		t.Fatalf("Flags are not being marked correctly")
+	}
+}
+
+// endregion
+
+// region NewNode
+func TestNewNodehasAttributesAttached(t *testing.T) {
+	node, err := NewNode(context.TODO(), &redis.Options{
+		Addr: "localhost:6379",
+	}, func(opt *redis.Options) *redis.Client {
+		db, mock := redismock.NewClientMock()
+		mock.ExpectClusterNodes().SetVal(`335e5ceff013eeebdbdb71bb65b4c1aeaf6a06f5 10.244.0.156:6379@16379 master - 0 1652373719041 2 connected
+9fd8800b31d569538917c0aaeaa5588e2f9c6edf 10.244.0.218:6379@16379 myself,master - 0 1652373716000 0 connected
+8a99a71a38d099de6862284f5aab9329d796c34f 10.244.0.160:6379@16379 master - 0 1652373718026 1 connected
+`)
+		return db
+	})
+	if err != nil {
+		t.Fatalf("Tried to create new node, but received error %v", err)
+	}
+	if node.NodeAttributes.ID != "9fd8800b31d569538917c0aaeaa5588e2f9c6edf" {
+		t.Fatalf("Wrong attributes attached for node")
+	}
+}
+
+// endregion
+
+// region GetSelfAttributes
+func TestNode_GetSelfAttributes(t *testing.T) {
+	db, mock := redismock.NewClientMock()
+	redisNode := Node{
+		Client: db,
+		clientBuilder: func(opt *redis.Options) *redis.Client {
+			client, _ := redismock.NewClientMock()
+			return client
+		},
+	}
+	mock.ExpectClusterNodes().SetVal(`335e5ceff013eeebdbdb71bb65b4c1aeaf6a06f5 10.244.0.156:6379@16379 master - 0 1652373719041 2 connected
+9fd8800b31d569538917c0aaeaa5588e2f9c6edf 10.244.0.218:6379@16379 myself,master - 0 1652373716000 0 connected
+8a99a71a38d099de6862284f5aab9329d796c34f 10.244.0.160:6379@16379 master - 0 1652373718026 1 connected
+`)
+	attributes, err := redisNode.GetSelfAttributes(context.TODO())
+	if err != nil {
+		t.Fatalf("Got error while trying to read my attributes %v", err)
+	}
+	if attributes.ID != "9fd8800b31d569538917c0aaeaa5588e2f9c6edf" {
+		t.Fatalf("Got info for the wrong node. Expected info for 9fd8800b31d569538917c0aaeaa5588e2f9c6edf, Got info for %s", attributes.ID)
+	}
+}
+
+// endregion
+
 // region GetFriends
 func TestRedisNodeGetFriendsReturnsKnowsNodes(t *testing.T) {
 	db, mock := redismock.NewClientMock()
@@ -84,4 +151,5 @@ func TestMeetNodeRunsNodeMeetForNewNode(t *testing.T) {
 		t.Fatalf("Not all of the required Redis commands were run")
 	}
 }
+
 // endregion
